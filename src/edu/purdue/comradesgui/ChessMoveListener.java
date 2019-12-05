cd5d2d41a10645828507ec0820b884f71de05a66
Kuhndowg <Kuhndowg@134.68.245.46>
2019-12-05 13:34:44 -0500

    Deleting all files

--- src/edu/purdue/comradesgui/ChessMoveListener.java
+++
@@ -1,8 +0,0 @@
-package edu.purdue.comradesgui;
-
-import java.util.EventListener;
-
-public interface ChessMoveListener extends EventListener {
-
-	void moveEvent(ChessPlayer ply, ChessMove move);
-}
--- src/module-info.java
+++
@@ -1,6 +0,0 @@
-module edu.purdue.comradesgui {
-	requires java.desktop;
-	requires javafx.graphics;
-	requires javafx.controls;
-	exports edu.purdue.comradesgui.javafx;
-}
