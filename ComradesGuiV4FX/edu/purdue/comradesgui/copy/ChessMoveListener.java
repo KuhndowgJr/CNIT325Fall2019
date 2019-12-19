c735fe676d43e3699059cd2376764a75c9c96125
Pavithra Yendluru <yendluru@usc.edu>
2019-12-19 13:51:41 -0500

    Final commit

--- ComradesGuiV4FX/edu/purdue/comradesgui/copy/ChessMoveListener.java
+++
@@ -1,8 +0,0 @@
-package edu.purdue.comradesgui.copy;
-
-import java.util.EventListener;
-
-public interface ChessMoveListener extends EventListener {
-
-	void moveEvent(ChessPlayer ply, ChessMove move);
-}
